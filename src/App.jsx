633ae3905fd9235537634c7b3eae27a4606f8af0
qxqxia <qxqinxia@outlook.com>
2023-08-31 11:35:28 +0200

    cat

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function App() {
-    const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4']);
+// function App() {
+//     const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4']);
 
-    const addItem = () => {
-        const newItem = `New Item ${items.length + 1}`;
-        setItems([...items, newItem]);
-    };
+//     const addItem = () => {
+//         const newItem = `New Item ${items.length + 1}`;
+//         setItems([...items, newItem]);
+//     };
 
-    return (
-        <div>
-            <h1>Dynamic Webpage with React</h1>
-            <button onClick={addItem}>Add Item</button>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+//     return (
+//         <div>
+//             <h1>Dynamic Webpage with React</h1>
+//             <button onClick={addItem}>Add Item</button>
+//             <ul>
+//                 {items.map((item, index) => (
+//                     <li key={index}>{item}</li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
 
-export default App;
+// export default App;
 
 // function App() {
 
@@ -33,3 +33,19 @@ export default App;
 // }
 
 // export default App
+
+import Cat from './Cat.jsx'
+
+function App() {
+
+    return (
+        <>
+            <div style={{display:'gird', placeItems:'center'}}>
+                <h1>hello world</h1>
+            </div>
+            <Cat />
+        </>
+    )
+}
+
+export default App
