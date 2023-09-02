47a55f63dc51af6be1c888205c634d24aad871f6
qx <96128825+qxqxia@users.noreply.github.com>
2023-09-02 14:39:23 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,4 @@
 import React, { useState } from 'react';
-
-// function App() {
-//     const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4']);
-
-//     const addItem = () => {
-//         const newItem = `New Item ${items.length + 1}`;
-//         setItems([...items, newItem]);
-//     };
-
-//     return (
-//         <div>
-//             <h1>Dynamic Webpage with React</h1>
-//             <button onClick={addItem}>Add Item</button>
-//             <ul>
-//                 {items.map((item, index) => (
-//                     <li key={index}>{item}</li>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-// }
-
-// export default App;
-
-// function App() {
-
-//   return (
-//     <>
-//       hello world
-//     </>
-//   )
-// }
-
-// export default App
-
 import Cat from './Cat.jsx'
 import Photo from './Photo.jsx'
 
